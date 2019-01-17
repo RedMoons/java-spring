e0b2e045c3f8d9e4d023d85bf55435ad5cda0d6b
Seongjin Byeon <sjbyeon610@gmail.com>
2019-01-17 23:12:42 +0900

    restful-web-services course 05

--- restful-web-services/src/main/java/com/sj28study/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/sj28study/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.sj28study.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,17 @@ public class HelloWorldController {
 	public String hellowWorld() {
 		return "Hello world";
 	}
+
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean hellowWorldBean() {
+		return new HelloWorldBean("Hello world");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean hellowWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world, %s", name));
+	}
+
+
 
 }
